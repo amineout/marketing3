fad017e82508cc649d04e3a8453503a7072759b7
amineout <111193634+amineout@users.noreply.github.com>
2022-08-18 18:14:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+function boton(){
+    alert("wach jhelti matb9ach gha katwrek rekz");
+
+}
